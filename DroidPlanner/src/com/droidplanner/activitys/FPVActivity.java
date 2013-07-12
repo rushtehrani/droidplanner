ebf087f0bd65b3bc9f752829e9df2fc049815c6e
Arthur Benemann <arthur.benemann@gmail.com>
2013-07-12 18:09:35 -0300

    Streaming of a IP camera with MJPEG data working.

--- DroidPlanner/src/com/droidplanner/activitys/FPVActivity.java
+++ DroidPlanner/src/com/droidplanner/activitys/FPVActivity.java
@@ -1,14 +1,31 @@
 package com.droidplanner.activitys;
 
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
 import com.droidplanner.R;
+import com.droidplanner.widgets.FPV.MjpegInputStream;
+import com.droidplanner.widgets.FPV.MjpegView;
 
 public class FPVActivity extends SuperActivity {
+
+	private MjpegView vv;
+
+	// sample public cam
+	//String URL = "http://trackfield.webcam.oregonstate.edu/axis-cgi/mjpg/video.cgi?resolution=800x600&amp%3bdummy=1333689998337";
+	String URL = "http://192.168.40.143:81/videostream.cgi?user=admin&pwd=&resolution=32&rate=10";
+	
 
 	@Override
 	int getNavigationItem() {
@@ -16,20 +33,9 @@ public class FPVActivity extends SuperActivity {
 	}
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		VideoView vv = new VideoView(this);
-        setContentView(vv);        
-        vv.setVideoPath("http://192.168.40.125:8080/");
-        vv.setMediaController(new MediaController(this));        
-
-	}
-
-	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		switch (item.getItemId()) {
-			default:
+		default:
 			return super.onMenuItemSelected(featureId, item);
 		}
 	}
@@ -38,5 +44,59 @@ public class FPVActivity extends SuperActivity {
 		getMenuInflater().inflate(R.menu.menu_fpv, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.fpv);
 		
+		vv = (MjpegView) findViewById(R.id.mjpegView1);
+		new DoRead().execute(URL);
+
+	}
+
+	public void onPause() {
+		super.onPause();
+		vv.stopPlayback();
+	}
+
+	public class DoRead extends AsyncTask<String, Void, MjpegInputStream> {
+		private static final String TAG = "FPV";
+
+		protected MjpegInputStream doInBackground(String... url) {
+			// TODO: if camera has authentication deal with it and don't just
+			// not work
+			HttpResponse res = null;
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			Log.d(TAG, "1. Sending http request");
+			try {
+				res = httpclient.execute(new HttpGet(URI.create(url[0])));
+				Log.d(TAG, "2. Request finished, status = "
+						+ res.getStatusLine().getStatusCode());
+				if (res.getStatusLine().getStatusCode() == 401) {
+					// You must turn off camera User Access Control before this
+					// will work
+					return null;
+				}
+				return new MjpegInputStream(res.getEntity().getContent());
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+				Log.d(TAG, "Request failed-ClientProtocolException", e);
+				// Error connecting to camera
+			} catch (IOException e) {
+				e.printStackTrace();
+				Log.d(TAG, "Request failed-IOException", e);
+				// Error connecting to camera
+			}
+
+			return null;
+		}
+
+		protected void onPostExecute(MjpegInputStream result) {
+			vv.setSource(result);
+			vv.setDisplayMode(MjpegView.SIZE_BEST_FIT);
+			vv.showFps(true);
+		}
+	}
 }
